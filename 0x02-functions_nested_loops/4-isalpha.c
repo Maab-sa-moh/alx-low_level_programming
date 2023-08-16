d30eccea68f2d7e9093f5c021c33db905029ef5e
Maab-sa-moh <maabelsayed88@gmail.com>
2023-08-15 19:15:05 -0700

    all tsks

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for alphabetic character
+ * @c: the character to be checked
+ * Return: 1 if x  is a letter, 0 otherwise
+ */
+int _isalpha(int x)
+{
+	return ((x >= 'a' && x <= 'z') || (x >= 'A' && x <= 'Z'));
+}
+
